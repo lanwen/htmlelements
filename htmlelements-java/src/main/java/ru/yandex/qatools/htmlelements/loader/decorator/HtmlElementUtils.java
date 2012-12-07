bd06a77d0f6725a47586aa05712a1cfe91a3cd0a
Artem Koshelev <artkoshelev@yandex-team.ru>
2012-12-08 00:44:43 +0400

    selenium 2.27.0

--- htmlelements-java/src/main/java/ru/yandex/qatools/htmlelements/loader/decorator/HtmlElementUtils.java
+++ htmlelements-java/src/main/java/ru/yandex/qatools/htmlelements/loader/decorator/HtmlElementUtils.java
@@ -1,18 +1,19 @@
 package ru.yandex.qatools.htmlelements.loader.decorator;
 
-import org.apache.commons.lang.WordUtils;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
-import ru.yandex.qatools.htmlelements.annotations.Block;
-import ru.yandex.qatools.htmlelements.annotations.Name;
-import ru.yandex.qatools.htmlelements.element.HtmlElement;
-import ru.yandex.qatools.htmlelements.element.TypifiedElement;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.List;
+
+import org.apache.commons.lang3.text.WordUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+
+import ru.yandex.qatools.htmlelements.annotations.Block;
+import ru.yandex.qatools.htmlelements.annotations.Name;
+import ru.yandex.qatools.htmlelements.element.HtmlElement;
+import ru.yandex.qatools.htmlelements.element.TypifiedElement;
 
 /**
  * Contains utility methods that are used by initialization mechanism.
